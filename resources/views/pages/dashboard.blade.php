8a1ef8529bb0e6e67e41c59b010007a12e11fc61
sunny panchal <panchal.sun15@gmail.com>
2024-04-11 01:41:50 +0530

    test change

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -160,7 +160,7 @@
     <div class="col-lg-8 d-flex align-items-stretch">
       <div class="card w-100">
         <div class="card-body p-4">
-          <h5 class="card-title fw-semibold mb-4">Recent Transactions</h5>
+          <h5 class="card-title fw-semibold mb-4">Recent Transactions ...</h5>
           <div class="table-responsive">
             <table class="table text-nowrap mb-0 align-middle">
               <thead class="text-dark fs-4">
